b864c7ab9ccc9759e61c28bfb7d7fbdbfdc6a1b8
berkeoz <84924357+berkeoz@users.noreply.github.com>
2022-09-04 17:01:54 +0000

    Delete packages/mssql/regular/PCK-1 directory

--- packages/mssql/regular/PCK-1/dbm_bt_prj2_rs1/upgrade/01_createAlbum.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[Album]
-(
-    [AlbumId] INT NOT NULL,
-    [Title] NVARCHAR(160) NOT NULL,
-    [ArtistId] INT NOT NULL,
-    CONSTRAINT [PK_Album] PRIMARY KEY CLUSTERED ([AlbumId])
-);
