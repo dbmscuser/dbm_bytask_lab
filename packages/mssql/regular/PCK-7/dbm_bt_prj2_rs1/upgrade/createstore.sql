b6da3cfdb0f078702bbf8a1dfcb9afa0aea905ab
berkeoz <84924357+berkeoz@users.noreply.github.com>
2023-03-11 08:23:01 -0500

    Update createstore.sql

--- packages/mssql/regular/PCK-7/dbm_bt_prj2_rs1/upgrade/createstore.sql
+++ packages/mssql/regular/PCK-7/dbm_bt_prj2_rs1/upgrade/createstore.sql
@@ -1,6 +1,5 @@
 CREATE TABLE [dbo].[store]
 (
-    [GenreId] INT NOT NULL,
-    [Name] NVARCHAR(120),
-    CONSTRAINT [PK_Genre] PRIMARY KEY CLUSTERED ([GenreId])
+    [Name] NVARCHAR(120)
+    
 );
