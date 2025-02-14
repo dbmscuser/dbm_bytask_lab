129e231428526312bc9224eb35530c0ea9149178
dbmscuser <dbm@dbmaestro.com>
2025-02-14 14:17:16 +0000

    Remove Use ssl

--- vars/dbmBuild.groovy
+++ vars/dbmBuild.groovy
@@ -1,6 +1,6 @@
 def call(String javaCmd, String projectName, String taskName, String server, String authType, String useSSL, String dbmCredentials){
   helpMsgbox("BUilding Package")
   withCredentials([usernamePassword(credentialsId: dbmCredentials, usernameVariable: 'username', passwordVariable: 'token')]){
-    bat "${javaCmd} -Build -ProjectName ${projectName}  -EnvName Dev_Env_1 -VersionType Tasks -AdditionalInformation ${taskName} -CreatePackage True  -PackageName ${taskName} -CreateDowngradeScripts True  -Server ${server} -AuthType ${authType} -UseSSL ${useSSL}" + ' -UserName %username% -Password %token%'
+    bat "${javaCmd} -Build -ProjectName ${projectName}  -EnvName Dev_Env_1 -VersionType Tasks -AdditionalInformation ${taskName} -CreatePackage True  -PackageName ${taskName} -CreateDowngradeScripts True  -Server ${server} -AuthType ${authType} " + ' -UserName %username% -Password %token%'
   }
 }
