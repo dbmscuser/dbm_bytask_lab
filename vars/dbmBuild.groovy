3d1e8b03e73c178cd7accb5511f8ce146f1e129b
dbmscuser <dbm@dbmaestro.com>
2025-02-14 14:09:25 +0000

    Remove filePath

--- vars/dbmBuild.groovy
+++ vars/dbmBuild.groovy
@@ -1,5 +1,5 @@
 def call(String javaCmd, String projectName, String taskName, String server, String authType, String useSSL, String dbmCredentials){
-  helpMsgbox("BUilding Package ${filePath}")
+  helpMsgbox("BUilding Package")
   withCredentials([usernamePassword(credentialsId: dbmCredentials, usernameVariable: 'username', passwordVariable: 'token')]){
     bat "${javaCmd} -Build -ProjectName ${projectName}  -EnvName Dev_Env_1 -VersionType Tasks -AdditionalInformation ${taskName} -CreatePackage True  -PackageName ${taskName} -CreateDowngradeScripts True  -Server ${server} -AuthType ${authType} -UseSSL ${useSSL}" + ' -UserName %username% -Password %token%'
   }
